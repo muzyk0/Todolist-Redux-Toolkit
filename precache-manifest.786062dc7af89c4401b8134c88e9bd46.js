200a754ccd2ebeb37d683e5da40dcb40f734e25b
muzyk0 <ru9art@gmail.com>
2021-03-07 10:10:37 +0300

    Updates

---
+++ precache-manifest.786062dc7af89c4401b8134c88e9bd46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47a2b4ef650180619f3809926c8c7c4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e24d39c68e50ea0fc2d8",
+    "url": "/todolist/static/css/main.30e68f72.chunk.css"
+  },
+  {
+    "revision": "53d6cd089744677fc35b",
+    "url": "/todolist/static/js/2.ad4ad9ed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.ad4ad9ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24d39c68e50ea0fc2d8",
+    "url": "/todolist/static/js/main.3623f821.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  },
+  {
+    "revision": "89e669d788ba2ffd743872b48ba870f0",
+    "url": "/todolist/static/media/girl_with_computer.89e669d7.jpg"
+  }
+]);
